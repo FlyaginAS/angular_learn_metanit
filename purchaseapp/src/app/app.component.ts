13e75c0860facf89401f98574ba7f744e6e73f2a
FlyaginAS <rus.reg.36@gmail.com>
2021-07-25 14:31:42 +0300

    привязка

--- purchaseapp/src/app/app.component.ts
+++ purchaseapp/src/app/app.component.ts
@@ -1,38 +1,34 @@
 import { Component } from '@angular/core';
-     
-class Item{
-    purchase: string;
-    done: boolean;
-    price: number;
-     
-    constructor(purchase: string, price: number) {
-  
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
-    }
-}
- 
+    
 @Component({
     selector: 'purchase-app',//куда отрисовать
     templateUrl: './app.component.html',
     styleUrls:['./app.component.css'],
 })
+
+
 export class AppComponent { 
-    text: string = "";
-    price: number = 0;
-     
-    items: Item[] = 
-    [
-        { purchase: "Хлеб", done: false, price: 15.9 },
-        { purchase: "Масло", done: false, price: 60 },
-        { purchase: "Картофель", done: true, price: 22.6 },
-        { purchase: "Сыр", done: false, price:310 }
-    ];
-    addItem(text: string, price: number): void {
-         
-        if(text==null || text.trim()=="" || price==null)
-            return;
-        this.items.push(new Item(text, price));
-    }
+
+  count:number=0;
+  important:boolean=false;
+  name:string;
+  isRed:boolean=false;
+
+  increaseCount($event:any):void{
+    this.count++;
+    console.log($event);
+  }
+
+  toggleImportant():void{
+    this.important=!this.important;
+    console.log(`important is ${this.important}`)
+  }
+
+  doRed():void{
+    this.isRed=!this.isRed;
+  }
+
+    
+    
+    
 }
